745671c900cb7ced59e0b7068e424a72b7dcbc77
woomir <woomir@gmail.com>
2021-03-30 22:37:22 +0900

    자리가 날때까지 반복 및 대저와 삼락 합치기

--- camping_crawling.py
+++ camping_crawling.py
@@ -22,14 +22,14 @@ driver = webdriver.Chrome(
 
 # 대저 캠핑장
 # =========================================================================
-url = 'https://www.daejeocamping.com/Camp.mobiz?camptype=camp01'
-
-driver.get(url)
-time.sleep(0.5)
-
 sendMessageCount = 0
 
 while sendMessageCount == 0:
+
+    url = 'https://www.daejeocamping.com/Camp.mobiz?camptype=camp01'
+    driver.get(url)
+    time.sleep(0.5)
+
     # Today를 기준으로 그 이후의 주말만 검색하기
     xpath = "//input[@id='resdate']"
     driver.find_element_by_xpath(xpath).click()
@@ -110,21 +110,109 @@ while sendMessageCount == 0:
         driver.find_element_by_xpath(xpath).click()
         time.sleep(0.1)
 
-    # 20초마다 실행
-    time.sleep(20)
+    # # 이번달로 다시 넘어가기
+    # xpath = "//input[@id='resdate']"
+    # driver.find_element_by_xpath(xpath).click()
+    # time.sleep(0.1)
+    # xpath = "//a[@data-handler='prev']"
+    # driver.find_element_by_xpath(xpath).click()
+    # time.sleep(0.1)
+    # ==========================================================
 
-    # 이번달로 다시 넘어가기
+    # 삼락 캠핑장
+    url = 'http://www.nakdongcamping.com/reservation/real_time'
+
+    driver.get(url)
+    time.sleep(0.5)
+
+    # 이번달
     xpath = "//input[@id='resdate']"
     driver.find_element_by_xpath(xpath).click()
     time.sleep(0.1)
 
-    xpath = "//a[@data-handler='prev']"
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    res = soup.select('table.ui-datepicker-calendar>tbody>tr>td')
+
+    s = [6, 13, 20, 27, 34]
+
+    for bb in s:
+        weekNumber = s.index(bb) + 1
+
+        if 'ui-state-disabled' not in res[bb]['class']:
+            xpath = "//input[@id='resdate']"
+            driver.find_element_by_xpath(xpath).click()
+            time.sleep(0.1)
+
+            xpath = "//*[@id='ui-datepicker-div']/table/tbody/tr[" + \
+                str(weekNumber)+"]/td[7]"
+            driver.find_element_by_xpath(xpath).click()
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            tagSelect = soup.select('div.click_inner')
+            activeSite = soup.find_all('a', 'cbtn_on')
+            siteInfo = []
+            count = 0
+
+            for title in activeSite:
+                if "area_c" not in title["class"]:
+                    siteInfo.append(title)
+                    count += 1
+
+            if count > 0:
+                telegramSendMessage('이번달', weekNumber, count, '삼락캠핑장')
+                print('삼락캠핑장: 이번달 ' + str(weekNumber) +
+                      '주차에' + str(count) + '개 사이트 예약 가능')
+                sendMessageCount += 1
+            else:
+                print('삼락캠핑장: 이번달 ' + str(weekNumber) + '주차 변동없음')
+
+        xpath = "//input[@id='resdate']"
+        driver.find_element_by_xpath(xpath).click()
+        time.sleep(0.1)
+
+    # 다음 달
+    xpath = "//a[@data-handler='next']"
     driver.find_element_by_xpath(xpath).click()
     time.sleep(0.1)
 
-# 텔레그램에서 그룹방 별 봇 아이디 확인 방법 (chat bot name: positioncheck), 그룹방 안에서 /start로 bot을 시작해줘야 함.
-# chat_token = "1752254532:AAHM8-RftUAr3V5KRJ2SzaBp41G8JTTeHIE"
-# chat = telegram.Bot(token=chat_token)
-# updates = chat.getUpdates()
-# for u in updates:
-#     print(u.message['chat']['id'])
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    res = soup.select('table.ui-datepicker-calendar>tbody>tr>td')
+
+    s = [6, 13, 20, 27, 34]
+
+    for bb in s:
+        weekNumber = s.index(bb) + 1
+
+        if 'ui-state-disabled' not in res[bb]['class']:
+            xpath = "//input[@id='resdate']"
+            driver.find_element_by_xpath(xpath).click()
+            time.sleep(0.1)
+
+            xpath = "//*[@id='ui-datepicker-div']/table/tbody/tr[" + \
+                str(weekNumber)+"]/td[7]"
+            driver.find_element_by_xpath(xpath).click()
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            tagSelect = soup.select('div.click_inner')
+            activeSite = soup.find_all('a', 'cbtn_on')
+            siteInfo = []
+            count = 0
+
+            for title in activeSite:
+                if "area_c" not in title["class"]:
+                    siteInfo.append(title)
+                    count += 1
+
+            if count > 0:
+                telegramSendMessage('다음달', weekNumber, count, '삼락캠핑장')
+                print('삼락캠핑장:다음달 ' + str(weekNumber) +
+                      '주차에' + str(count) + '개 사이트 예약 가능')
+                sendMessageCount += 1
+            else:
+                print('삼락캠핑장:다음달 ' + str(weekNumber) + '주차 변동없음')
+    # 60초마다 실행
+    time.sleep(10)
